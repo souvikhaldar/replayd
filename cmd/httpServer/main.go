5d5e3aff877b4166f2f8d1f249c1545c16dcec82
Souvik Haldar <souvik.haldar@kartbites.com>
2019-01-29 23:02:21 +0530

    added return

--- cmd/httpServer/main.go
+++ cmd/httpServer/main.go
@@ -23,22 +23,26 @@ func StoreInBuffer(w http.ResponseWriter, r *http.Request) {
 	body, er := ioutil.ReadAll(r.Body)
 	if er != nil {
 		http.Error(w, er.Error(), 500)
+		return
 	}
 	log.Println("Body recieved: ", string(body))
 	mux := &sync.Mutex{}
 	mux.Lock()
 	if _, e := b.Write(body); e != nil {
 		http.Error(w, e.Error(), 500)
+		return
 	}
 	mux.Unlock()
 	fmt.Println("Inserted: ", string(body))
 	fmt.Fprintf(w, "Successfully inserted data to buffer")
+	return
 }
 
 // ReadFromBuffer responds with the in-memory buffer data
 func ReadFromBuffer(w http.ResponseWriter, r *http.Request) {
 	log.Println("--Running in ReadFromBuffer--")
 	fmt.Fprintf(w, b.String())
+	return
 }
 func main() {
 	router := mux.NewRouter()
